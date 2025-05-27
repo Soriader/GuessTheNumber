e4bb40307a4fb522d6a29a33049c18b4d4c7eed7
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-05-27 12:39:30 +0200

    [~] Renamed method 'SoClose' to 'VeryClose', added constructor for random win number, first logic to 'GuessUserPanel()'

--- GuessTheNumber/GuessMethods.cs
+++ GuessTheNumber/GuessMethods.cs
@@ -2,22 +2,24 @@
 
 public class GuessMethods
 {
-    public int RandomNumberCreator()
+    private readonly int _winNumber;
+
+    public GuessMethods()
     {
-        return new Random().Next(1, 1001);
+        _winNumber = new Random().Next(1, 1001);
     }
 
-    public bool WinTheGame(int winNumber, int guessNumber)
+    public bool WinTheGame(int guessNumber)
     {
-        if (winNumber > guessNumber)
+        if (_winNumber > guessNumber)
         {
-            SoClose(winNumber, guessNumber);
+            VeryClose(_winNumber, guessNumber);
             Console.WriteLine("Your number is too low!");
             return false;
         }
-        else if (winNumber < guessNumber)
+        else if (_winNumber < guessNumber)
         {
-            SoClose(winNumber, guessNumber);
+            VeryClose(_winNumber, guessNumber);
             Console.WriteLine("Your number is too high!");
             return false;
         }
@@ -25,19 +27,29 @@ public class GuessMethods
         return true;
     }
 
-    private bool SoClose(int winNumber, int guessNumber)
+    private bool VeryClose(int winNumber, int guessNumber)
     {
         if (winNumber > guessNumber && winNumber - guessNumber <= 20)
         {
             Console.WriteLine("Your score is too low, but you're very close!");
-            return false;
+            return true;
         }
         else if (winNumber < guessNumber && guessNumber - winNumber <= 20)
         {
             Console.WriteLine("Your score is too high, but you're very close!");
-            return false;
+            return true;  
         }
         
-        return true;
+        return false;
+    }
+
+    public bool IsNotCorrectNumber(string userAnswer)
+    {
+        if(int.TryParse(userAnswer, out int number) && int.Parse(userAnswer) >= 1 && int.Parse(userAnswer) <= 1000)
+        {
+            return true;
+        }
+        
+        return false;
     }
 }
--- GuessTheNumber/MainPanel.cs
+++ GuessTheNumber/MainPanel.cs
@@ -2,8 +2,30 @@
 
 public class MainPanel
 {
+    public static GuessMethods GuessMethods = new GuessMethods();
     public string GuessUserPanel()
     {
-        return "You win!";
+        var userAnswer = Console.ReadLine();
+        var correctAnswer = int.TryParse(userAnswer, out int number);
+        int guessNumber = 0;
+        
+        if (!correctAnswer)
+        {
+            Console.WriteLine("Please enter a correct number from 1 to 1000");
+
+            while (!GuessMethods.IsNotCorrectNumber(userAnswer))
+            {
+                userAnswer = Console.ReadLine();
+            }
+            
+            guessNumber = int.Parse(userAnswer);
+        }
+
+        while (!GuessMethods.WinTheGame(guessNumber))
+        {
+            
+        }
+        
+        return $"You win! The number for guess is {guessNumber}";
     }
 }
